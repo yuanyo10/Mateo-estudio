804204acf80c99b3cfa9994c0957428e69d163d6
yuanyo10 <110678838+yuanyo10@users.noreply.github.com>
2026-06-07 12:48:10 +0200

    Create questions.ts

---
+++ lib/questions.ts
@@ -0,0 +1,6 @@
+export const questions = [
+  { word: "ca__a", answer: "b", hint: "Animal" },
+  { word: "vi__ir", answer: "v", hint: "Hacer vida" },
+  { word: "a__ión", answer: "v", hint: "Vuela" },
+  { word: "ha__lar", answer: "b", hint: "Hablar" }
+];
