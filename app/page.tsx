3fdae4e9e20e4f2820b1ddb7de0e2bf0d3bf23ba
yuanyo10 <110678838+yuanyo10@users.noreply.github.com>
2026-06-07 12:57:07 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <main style={{ padding: 20 }}>
       <h1>🎓 Mateo Grammar Quest</h1>
+      <p>Aprende B y V jugando</p>
+
       <a href="/game">▶ Jugar</a>
     </main>
   );
